82b3ddf175eba65cb22bc82abb911f19c8c983fb
fefenfeng <1369508762@qq.com>
2023-07-06 20:14:05 +0100

    torchvision dataset的使用

--- P10_dataset_transform.py
+++ P10_dataset_transform.py
@@ -5,10 +5,15 @@ dataset_transform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor()
 ])
 
+# root根目录配置，train bool值表明是否训练集
 train_set = torchvision.datasets.CIFAR10(root="./dataset", train=True, transform=dataset_transform, download=True)
 test_set = torchvision.datasets.CIFAR10(root="./dataset", train=False, transform=dataset_transform, download=True)
+# train_set = torchvision.datasets.CIFAR10(root="./dataset", train=True, download=True)
+# test_set = torchvision.datasets.CIFAR10(root="./dataset", train=False, download=True)
 
+# CIFAR 60000 32*32
 # print(test_set[0])
+# print(train_set[0])
 # print(test_set.classes)
 #
 # img, target = test_set[0]
@@ -19,7 +24,7 @@ test_set = torchvision.datasets.CIFAR10(root="./dataset", train=False, transform
 #
 # print(test_set[0])
 
-writer = SummaryWriter("p10")
+writer = SummaryWriter("logs")  #这里就只是个文件夹名字
 for i in range(10):
     img, target = test_set[i]
     writer.add_image("test_set", img, i)
