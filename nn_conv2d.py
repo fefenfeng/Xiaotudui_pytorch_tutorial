016edbe9de64cabc41cb478555fd39eaa4fc4c41
fefenfeng <1369508762@qq.com>
2023-07-07 18:46:27 +0100

    Conv2d

--- nn_conv2d.py
+++ nn_conv2d.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# 作者：小土堆
-# 公众号：土堆碎念
 import torch
 import torchvision
 from torch import nn
@@ -8,27 +5,28 @@ from torch.nn import Conv2d
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-dataset = torchvision.datasets.CIFAR10("../data", train=False, transform=torchvision.transforms.ToTensor(),
+dataset = torchvision.datasets.CIFAR10("./dataset", train=False, transform=torchvision.transforms.ToTensor(),
                                        download=True)
 dataloader = DataLoader(dataset, batch_size=64)
 
-class Tudui(nn.Module):
+class Fengmodule(nn.Module):
     def __init__(self):
-        super(Tudui, self).__init__()
+        super(Fengmodule, self).__init__()
         self.conv1 = Conv2d(in_channels=3, out_channels=6, kernel_size=3, stride=1, padding=0)
 
     def forward(self, x):
         x = self.conv1(x)
         return x
 
-tudui = Tudui()
+fengmodel = Fengmodule()
+# print(fengmodel)
 
-writer = SummaryWriter("../logs")
+writer = SummaryWriter("./logs")
 
 step = 0
 for data in dataloader:
     imgs, targets = data
-    output = tudui(imgs)
+    output = fengmodel(imgs)
     print(imgs.shape)
     print(output.shape)
     # torch.Size([64, 3, 32, 32])
